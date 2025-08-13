8767d38c0cf5ed6fc80cb52d1a3da00db903013c
ych <2434512633@qq.com>
2025-08-13 16:46:53 +0800

    allure

--- pytest-pip-main/test/allure_attachments_test.py
+++ pytest-pip-main/test/allure_attachments_test.py
@@ -3,10 +3,11 @@ import os
 
 import allure
 from allure import attachment_type
+from loguru import logger
 
 
 def test_attach():
-    allure.attach.file(os.path.join("resources", "allure-logo.png"), name="PNG example",
+    allure.attach.file(os.path.join("..", "resources", "allure-logo.png"), name="PNG example",
                        attachment_type=attachment_type.PNG)
 
     allure.attach("<h1>Example html attachment</h1>", name="HTML example", attachment_type=attachment_type.HTML)
@@ -17,3 +18,14 @@ def test_attach():
     allure.attach("\n".join(["https://github.com/allure-framework", "https://github.com/allure-examples"]),
                   name="URI List example", attachment_type=attachment_type.URI_LIST)
     # TODO Screen diff
+
+
+def test_log():
+    """基础使用"""
+    logger.debug("调试信息")
+    logger.info("业务提示")
+    logger.info("详细提示")
+    logger.warning("警告提醒")
+    logger.error("错误追踪")
+    logger.critical("致命错误")
+    assert True
